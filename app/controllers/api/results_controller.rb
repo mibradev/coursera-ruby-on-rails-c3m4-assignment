e1f8262f4de46f8e4293dfeba8342d73c313e921
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-01-11 10:01:01 +0200

    Resource Implementation (JSON)

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -3,12 +3,22 @@ module Api
     def index
       unless request.accept && request.accept != "*/*"
         render plain: "/api/races/#{params[:race_id]}/results"
+      else
+        @race = Race.find(params[:race_id])
+        @entrants = @race.entrants
       end
     end
 
     def show
       unless request.accept && request.accept != "*/*"
         render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
+      else
+        respond_to do |format|
+          @race = Race.where(id: params[:race_id]).first
+          @result = @race.entrants.where(id: params[:id]).first if @race
+
+          format.json { render :show } if @result
+        end
       end
     end
 
@@ -22,6 +32,38 @@ module Api
     end
 
     def update
+      result = params[:result]
+
+      if result
+        entrant = Race.find(params[:race_id]).entrants.find(params[:id])
+
+        if result[:swim]
+          entrant.swim = entrant.race.race.swim
+          entrant.swim_secs = result[:swim].to_f
+        end
+
+        if result[:t1]
+          entrant.t1 = entrant.race.race.t1
+          entrant.t1_secs = result[:t1].to_f
+        end
+
+        if result[:bike]
+          entrant.bike = entrant.race.race.bike
+          entrant.bike_secs = result[:bike].to_f
+        end
+
+        if result[:t2]
+          entrant.t2 = entrant.race.race.t2
+          entrant.t2_secs = result[:t2].to_f
+        end
+
+        if result[:run]
+          entrant.run = entrant.race.race.run
+          entrant.run_secs = result[:run].to_f
+        end
+
+        render nothing: true if entrant.save
+      end
     end
 
     def destroy
---
+++ app/views/api/results/index.json.jbuilder
@@ -0,0 +1,2 @@
+json.ignore_nil!
+json.array! @entrants, partial: "result", as: :result
